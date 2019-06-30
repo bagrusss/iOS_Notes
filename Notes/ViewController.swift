a1a7594dd34187a4dc0c2b9efd7118f1149dcb95
Vladislav Kozhushko <bagrusss@gmail.com>
2019-06-30 17:59:47 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Vladislav Kozhushko on 30/06/2019.
+//  Copyright © 2019 bagrusss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
